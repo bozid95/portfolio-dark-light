7e3dc4b3d2199a69b291a7c6c278882cc70b957b
Widodo <widodroid@gmail.com>
2022-09-24 23:13:21 +0700

    Add files via upload

---
+++ post.js
@@ -0,0 +1,23 @@
+$.ajax({
+  type: "GET",
+  url: "https://www.googleapis.com/blogger/v3/blogs/3202973009101031702/posts?key=AIzaSyA7s8tE-8FX_39ahUqxK1fUsIBuz43sUZk",
+  data: {},
+  success: function (result) {
+    var item = result.items;
+    $.each(item, function (i, data) {
+      $("#tampil").append(
+        '<p><button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample' +
+          i +
+          '" aria-expanded="false" aria-controls="collapseWidthExample' +
+          i +
+          '">' +
+          data.title +
+          '</button></p><div style="min-height:auto;"><div class="collapse collapse-horizontal" id="collapseWidthExample' +
+          i +
+          '"> <div class="card card-body" style="width: auto;">' +
+          data.content +
+          "</div> </div></div>"
+      );
+    });
+  },
+});
